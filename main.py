47fb0d38d628fb65f9602dc2debc2dd22ada5974
MajorThird <test@testtesttest.org>
2020-02-05 21:59:22 +0100

    Make it display the images for each layer.

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ import cv2
 import numpy as np
 import random
 import math
+import connections
+
+def get_empty_white_canvas(size_x=1920, size_y=1080):
+    img = np.array([255], dtype=np.uint8) * np.ones((size_y,size_x,1), dtype=np.uint8)
+    return img
 
 
 def get_prepared_image(source_image, scale_factor):
@@ -18,13 +23,72 @@ def get_prepared_image(source_image, scale_factor):
     return resized_image
 
 
+def get_point_thresholds(no_of_layers, exponent, prefactor):
+    return [math.pow(x, exponent) * prefactor for x in range(no_of_layers)]
+
+
+def get_layer_points(current_max, point_threshold, prepared_image):
+    """
+    This function returns the points we will
+    later connect with line segments.
+    Random points may be put anywhere where the lightness
+    of the image is below current_max. For each
+    pixel that may potentially represent a point, we draw
+    a random number. A point is only created if the random number
+    if below point_threshold.
+    """
+    white_value = 255
+    layer = prepared_image.copy()
+    # find all positions where the image is darker than current_max
+    layer[layer <= current_max] = 0
+    layer[layer != 0] = white_value
+
+    random_matrix = np.random.rand(*prepared_image.shape)
+    layer[random_matrix > point_threshold] = white_value
+    points = np.argwhere(layer == 0)
+    points_tuples = [(p[1],p[0]) for p in points]
+    return points_tuples
 
 def create_scribble_art(config):
     source_image = cv2.imread(config["INPUT_OUTPUT"]["input_image"])
     scale_factor = float(config["DRAWING"]["image_scale_factor"])
     prepared_image = get_prepared_image(source_image, scale_factor)
-    cv2.imshow("test", prepared_image)
-    cv2.waitKey(0)
+
+    no_of_layers = int(config["DRAWING"]["no_of_layers"])
+    exponent = float(config["DRAWING"]["point_thresholds_exponent"])
+    prefactor = float(config["DRAWING"]["point_thresholds_prefactor"])
+    point_thresholds = get_point_thresholds(no_of_layers, exponent, prefactor)
+    gray_value_step = 255.0 / len(point_thresholds)
+
+    canvas = get_empty_white_canvas(prepared_image.shape[1], prepared_image.shape[0])
+    max_distance = float(config["DRAWING"]["max_line_length_factor"]) * min(canvas.shape[1], canvas.shape[0])
+    for layer_index in range(no_of_layers):
+        current_max = 255.0 - (layer_index + 1.0) * gray_value_step
+        points = get_layer_points(current_max, point_thresholds[layer_index], prepared_image)
+
+        if len(points) > 1:
+            xmax = prepared_image.shape[1]
+            ymax = prepared_image.shape[0]
+
+
+            connected = connections.connect_points(points, max_distance, xmax, ymax)
+
+
+            for i in range(len(connected)-1):
+                start = connected[i]
+                end = connected[i+1]
+                color = [0]
+                stroke_scale=1
+
+                if connections.calc_distance(start,end) < max_distance:
+                    # dwg.add(dwg.line(start, end, stroke=svgwrite.rgb(0, 0, 0, '%')))
+                    cv2.line(canvas, start, end, color, thickness=stroke_scale, lineType=8, shift=0)
+            cv2.imshow("test", canvas)
+            cv2.imwrite("./output/result_%04i.png" % layer_index, canvas)
+            cv2.waitKey(1)
+
+
+
 
 def delete_and_create_output_folder():
     """
